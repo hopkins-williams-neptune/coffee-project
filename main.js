74995db1680927bd404eecc62d64848c55047ffd
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-04-26 12:09:35 -0500

    coffee project

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    var html = '<div class="tr coffee">';
+    // html += '<div class="td">' + coffee.id + '</div>';
+    html += '<div class="td">' + coffee.name + '</div>';
+    html += '<div class="td">' + coffee.roast + '</div>';
+    html += '</div>';
 
     return html;
 }
@@ -13,6 +13,7 @@ function renderCoffee(coffee) {
 function renderCoffees(coffees) {
     var html = '';
     for(var i = coffees.length - 1; i >= 0; i--) {
+        //for(var i = 0; i <= coffees.length - 1; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
@@ -22,13 +23,36 @@ function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
+
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
+          filteredCoffees.push(coffee);
+        // }
+        } else if (selectedRoast === "any") {
             filteredCoffees.push(coffee);
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
+
+// function updateCoffeesText(e) {
+//   e.preventDefault(); // don't submit the form, we just want to update the data
+//   // var selectedRoast = roastSelection.value;
+//   var filteredCoffees = [];
+//   var textSearch = nameSelection.value;
+//   console.log(textSearch)
+//
+//   coffees.forEach(function(coffee) {
+//     if (coffee.roast === textSearch) {
+//       filteredCoffees.push(coffee);
+//       // }
+//     }
+//   });
+//   tbody.innerHTML = renderCoffees(filteredCoffees);
+// }
+
+
+
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
@@ -50,8 +74,35 @@ var coffees = [
 
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
+// var typeButton = document.querySelector('#submitType');
 var roastSelection = document.querySelector('#roast-selection');
+var nameSelection = document.querySelector("#submitType")
 
 tbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
+
+
+
+
+// typeButton.addEventListener('keydown', updateCoffees);
+
+//
+// function myFunction() {
+//   // Declare variables
+//   var input, filter, roast, name, txtValue;
+//   input = document.querySelector("#submitType");
+//   filter = input.value.toUpperCase();
+//   // roast = document.querySelector("#roast-selection");
+//   name =
+//   // Loop through all list items, and hide those who don’t match the search query
+//   for (i = 0; i < li.length; i++) {
+//     a = li[i].getElementsByTagName(“a”)[0];
+//     txtValue = a.textContent || a.innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       li[i].style.display = “”;
+//     } else {
+//       li[i].style.display = “none”;
+//     }
+//   }
+// }
