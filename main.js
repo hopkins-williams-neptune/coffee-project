5744d3014d12721630d71bbb17defceaff370442
Joseph Hopkins <joseph.r.hopkins88@gmail.com>
2021-04-27 11:34:36 -0500

    css mostly

--- main.js
+++ main.js
@@ -140,7 +140,7 @@ function updateCoffeesText(e) {
 }
 
 function getRandomQuote(){
-  var quotesArray = ["bada bing","You got bamboozled","Hi there","What ever"]
+  var quotesArray = ["Never trust anyone who doesn’t drink coffee. – AJ Lee","Coffee is a kind of magic you can drink. ― Catherynne M. Valente","Never underestimate the power of a good cup of coffee. – Ursula Vernon","At least there was coffee. Reliable, delicious, life-giving coffee. – Mary H.K. Choi"]
   return quotesArray[Math.floor(Math.random() * quotesArray.length - 1) + 1];
 }
 
