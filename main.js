de515308ab0fd3eb49f470f2dce93a549fdeb46c
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-04-27 14:01:04 -0500

    converting cards

--- main.js
+++ main.js
@@ -7,7 +7,22 @@
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 
 //make a blank array to hold our list of coffee objects
-var coffees = [];
+var coffees = [
+  {id: 1, name: 'Light City', roast: 'light'},
+  {id: 2, name: 'Half City', roast: 'light'},
+  {id: 3, name: 'Cinnamon', roast: 'light'},
+  {id: 4, name: 'City', roast: 'medium'},
+  {id: 5, name: 'American', roast: 'medium'},
+  {id: 6, name: 'Breakfast', roast: 'medium'},
+  {id: 7, name: 'High', roast: 'dark'},
+  {id: 8, name: 'Continental', roast: 'dark'},
+  {id: 9, name: 'New Orleans', roast: 'dark'},
+  {id: 10, name: 'European', roast: 'dark'},
+  {id: 11, name: 'Espresso', roast: 'dark'},
+  {id: 12, name: 'Viennese', roast: 'dark'},
+  {id: 13, name: 'Italian', roast: 'dark'},
+  {id: 14, name: 'French', roast: 'dark'},
+];
 
 function fillCoffeeArray()
 {
@@ -169,6 +184,7 @@ submitCoffee.addEventListener('click', addToCoffeeArray);
 
 submitButtonText.addEventListener('click', updateCoffeesText);
 
+
 //Text content, with changing the event listerner
 
 // typeButton.addEventListener('keydown', updateCoffees);
@@ -192,4 +208,4 @@ submitButtonText.addEventListener('click', updateCoffeesText);
 //     }
 //   }
 // }
-window.onload = fillCoffeeArray("");
+
